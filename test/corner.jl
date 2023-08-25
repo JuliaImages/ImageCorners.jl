ed06e929b6958df4fe4f51bc2971aaaf9bc2bff1
Jay-sanjay <134289328+Jay-sanjay@users.noreply.github.com>
2023-08-25 20:20:20 +0530

    changes in window size and atol

--- test/corner.jl
+++ test/corner.jl
@@ -386,14 +386,18 @@ using Test, ImageCorners
     @testset "Moravec Corner Detection" begin
         img = zeros(3, 3)
         img[1:1, 2:3] .= 1
-        img[1:2, 2:2] .= 0
-        corner_responses = moravec(img; window_size = 100)
-
+        img[1:2, 2:3] .= 0
+    
+        corner_responses = moravec(img; window_size = 8)
+    
         @testset "moravec" begin
+            @test isapprox(corner_responses[1, 1],maximum(corner_responses); atol = 0.001)
+            @test isapprox(corner_responses[1, 2],maximum(corner_responses); atol = 0.001)
+            @test isapprox(corner_responses[2, 1],maximum(corner_responses); atol = 0.001)
             @test isapprox(corner_responses[2, 2],maximum(corner_responses); atol = 0.001)
             @test isapprox(corner_responses[2, 3],maximum(corner_responses); atol = 0.001)
             @test isapprox(corner_responses[3, 2],maximum(corner_responses); atol = 0.001)
-            @test isapprox(corner_responses[3, 3],maximum(corner_responses); atol = 0.001)
+            @test isapprox(corner_responses[3, 3],maximum(corner_responses); atol = 0.0001)
         end
     end
 end  
